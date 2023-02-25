064d02fd6bc7cb8bb6e9f42cb87970701f23f146
Lujain-Alnouti <115452443+Lujain-Alnouti@users.noreply.github.com>
2023-02-25 20:58:23 +0300

    Project8

---
+++ ERP_System-main/ERP-Project-main/University_Registration/University_Registration/Controllers/mainHomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using University_Registration.Models;
+
+namespace University_Registration.Controllers
+{
+    public class mainHomeController : Controller
+    {
+        ERP_SystemEntities1 db = new ERP_SystemEntities1();
+
+        // GET: mainHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult coarses(int? id)
+        {
+
+            if (id == null)
+            {
+                var course = from m in db.Majors select m;
+                return View(course);
+            }
+            else
+            {
+                var course = from m in db.Majors where m.Facility_ID == id select m;
+                return View(course);
+            }
+            return View();
+        }
+        public ActionResult test()
+        {
+            return View();
+        }
+    }
+}
